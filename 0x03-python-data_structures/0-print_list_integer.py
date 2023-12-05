390075de76ee9d64e75b33ee2ccb85f61c45a80b
Newton Mwasomanya <mwasomanyan@gmail.com>
2023-12-05 19:13:58 +0200

    Data Structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-	for i in my_list:
-		print("{}".format(i))
+    for i in my_list:
+        if isinstance(i, int):
+            print("{}".format(i))
+        else:
+            pass
